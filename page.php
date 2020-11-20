4db6e90e09de0f6f88bd35156181c3ee20b1e23c
codingtutorialDE <codingtutorialde@gmail.com>
2020-11-20 03:24:34 +0100

    Part 8 - Bilder hinzugefügt und page.php weiter bearbeitet

--- page.php
+++ page.php
@@ -15,11 +15,6 @@
     </div>
 </section>
 <!-- hier endet die Überschrift unserer Seite -->
-<?php if (have_posts()) : while(have_posts()) : the_post();?>
-
-<?php the_content();?>
-
-<?php endwhile; endif;?>
 <!-- hier beginnt unser Text der Seite -->
 <section id="post-234" class="blog_area single-post-area section_padding">
     <div class="container">
@@ -27,163 +22,21 @@
             <div class="col-lg-8 posts-list">
                 <div class="single-post">
                     <div class="feature-img">
-                        <p> <strong>Einleitung</strong></p>
-                        <p>Möchten Sie weitere Kommentare zu Ihren Blogbeiträgen erhalten? Wir werden oft nach Tipps
-                            gefragt, wie Kommentare zu WordPress-Blogbeiträgen erhöht werden können. In diesem Artikel
-                            werden 11 Möglichkeiten beschrieben, um weitere Kommentare zu Ihren WordPress-Blogbeiträgen
-                            zu erhalten.</p>
-                        <hr />
-                        <h2>1. Holen Sie sich ein besseres WordPress-Hosting</h2>
-                        <p>Kommentare sind ressourcenintensiv. Jedes Mal, wenn ein Benutzer einen Kommentar zu Ihrer
-                            WordPress-Site einreicht, führt Ihr Server ein PHP-Skript aus. Wenn mehrere Benutzer
-                            gleichzeitig Kommentare senden, kann dies Ihre Website verlangsamen.</p>
-                        <p>Sie müssen sicherstellen, dass Ihr WordPress-Hosting-Provider damit umgehen kann. Wenn Sie
-                            eine größere Website haben, versuchen Sie es mit einem verwalteten
-                            WordPress-Hostinganbieter, um die Geschwindigkeit zu verbessern.</p>
-                        <hr />
-                        <h2>2. Verwenden Sie die Kommentar-Moderation anstelle von Captcha</h2>
-                        <p>Das Hinzufügen von CAPTCHA zum Blockieren von Spam ist eine effektive Methode, um mit
-                            Kommentar-Spam umzugehen.</p>
-                        <p>CAPTCHA ist jedoch nicht benutzerfreundlich und hält reguläre Benutzer von der Kommentierung
-                            ab.</p>
-                        <p>Anstatt CAPTCHA zu verwenden, sollten Sie die Kommentarmoderation auf Ihrer Site aktivieren
-                            und jeden Kommentar manuell genehmigen (falls erforderlich).</p>
-                        <p>Dank der Moderation von Kommentaren können Sie nicht nur Spam blockieren, sondern auch die
-                            Möglichkeit haben, häufiger mit Ihren Benutzern zu interagieren.</p>
-                        <p>Ausführliche Anweisungen finden Sie in unserem Anfängerhandbuch, wie Kommentare in WordPress
-                            moderiert werden .</p>
-                        <h2>3. Erlauben Sie Benutzern, Kommentare zu abonnieren</h2>
-                        <p>Wenn ein Benutzer einen Kommentar auf Ihrer Website hinterlässt, muss er standardmäßig den
-                            Link mit einem Lesezeichen versehen, um zu sehen, ob ein neuer Kommentar zu dem Artikel
-                            vorliegt.</p>
-                        <p>Wenn sie vergessen, ein Lesezeichen zu setzen oder sich nicht an den Namen Ihres Blogs
-                            erinnern können, werden sie wahrscheinlich nie wieder zurückkehren.</p>
-                        <p>Sie können Benutzern die Möglichkeit geben, Kommentare zu Ihren WordPress-Beiträgen zu
-                            abonnieren. Auf diese Weise können sie sofort benachrichtigt werden, wenn ein neuer
-                            Kommentar vorliegt.</p>
-                        <p>Dadurch werden Ihre Seitenzugriffe und die Anzahl der Kommentare auf Ihrer Website erheblich
-                            erhöht. Ihre Benutzer finden auf Ihrer Website Diskussionen, die ansprechender sind.</p>
-                        <p><img class="size-full wp-image-533 aligncenter"
-                                src="../../storage.googleapis.com/stateless-www-seo-for-dummies/2018/12/813fb751-subscribetocomments.png"
-                                alt="" width="520" height="271" /></p>
-                        <p>In unserem Leitfaden erfahren Sie, wie Benutzer Kommentare in WordPress abonnieren können.
-                        </p>
-                        <hr />
-                        <h2>4. Benachrichtigen Sie Benutzer über Antworten auf ihre eigenen Kommentare</h2>
-                        <p>Wenn ein Benutzer einen Kommentar auf Ihrer Website hinterlässt, können Sie nicht
-                            herausfinden, ob jemand auf seinen Kommentar geantwortet hat. Sie müssen die Seite mit einem
-                            Lesezeichen versehen und sie später besuchen. Die meisten Benutzer werden das nicht tun.</p>
-                        <p>Sie können Benutzern leicht erlauben, ihre eigenen Kommentare zu abonnieren. Auf diese Weise
-                            erhalten sie NUR eine Benachrichtigungs-E-Mail, wenn jemand auf ihren Kommentar antwortet.
-                        </p>
-                        <p>Wenn Sie es sich zur Gewohnheit machen, auf die meisten Benutzerkommentare auf Ihrer Website
-                            zu antworten, könnte dies zu einer äußerst anregenden Community rund um Ihr Blog führen.</p>
-                        <hr />
-                        <h2>5. Neueste Kommentare zuerst anzeigen</h2>
-                        <p>Standardmäßig zeigt WordPress den neuesten Kommentar am unteren Rand der Kommentarliste. Wenn
-                            Sie jetzt viele Kommentare zu einem Beitrag haben, werden die ältesten Kommentare oben
-                            angezeigt.</p>
-                        <p>Dies kann leicht in WordPress geändert werden.</p>
-                        <p>Gehen Sie einfach zu <strong>Einstellungen »Diskussion</strong> und scrollen Sie nach unten
-                            zu den anderen Einstellungen. Sie müssen neuere aus dem Dropdown-Menü auswählen.</p>
-                        <p><img class="size-full wp-image-534 aligncenter"
-                                src="../../storage.googleapis.com/stateless-www-seo-for-dummies/2018/12/5a72df15-newcommentsfirst.png"
-                                alt="" width="520" height="232" /></p>
-                        <hr />
-                        <h2>6. Aktivieren Sie Kommentare für alte Beiträge</h2>
-                        <p>Die meisten Blogger deaktivieren Kommentare zu älteren Beiträgen, um Spam zu reduzieren. Es
-                            wird jedoch viele Benutzer geben, die diese Beiträge kommentieren möchten.</p>
-                        <p>Da Sie bereits die Kommentarmoderation verwenden, können Sie Kommentare zu alten Posts sicher
-                            aktivieren.</p>
-                        <p>Gehen Sie einfach zu <strong>Einstellungen »Diskussion</strong> und scrollen Sie nach unten
-                            zu den anderen Einstellungen. Deaktivieren Sie das Kontrollkästchen
-                            neben <em>&#8222;Kommentare für Artikel, die älter als X Tage sind, automatisch
-                                schließen&#8220;</em> .</p>
-                        <p><img class="alignnone size-full wp-image-535"
-                                src="../../storage.googleapis.com/stateless-www-seo-for-dummies/2018/12/fc4436aa-turnoncomments.png"
-                                alt="" width="520" height="238" /></p>
-                        <hr />
-                        <h2>7. Präsentieren Sie Ihre Top-Kommentatoren</h2>
-                        <p>Eine gute Möglichkeit, Ihre treusten Benutzer zu ermutigen und zu würdigen, ist die
-                            Hervorhebung Ihrer Top-Kommentatoren. Dies zeigt den Benutzern, dass sie Teil der Community
-                            Ihres Blogs sind, und Sie schätzen es, wenn Sie sich bemühen, Ihren Kommentaren Leben
-                            einzuhauchen.</p>
-                        <p>Installieren und aktivieren Sie einfach das Plugin für <a title="Top-Kommentatoren-Widget"
-                                href="https://wordpress.org/plugins/top-commentators-widget/" target="_blank"
-                                rel="nofollow noopener noreferrer">Top-Kommentatoren</a> . Gehen Sie nach der
-                            Aktivierung zur Seite <strong>Aussehen »Widgets</strong> und fügen Sie das
-                            Top-Kommentierer-Widget zu einer Seitenleiste hinzu.</p>
-                        <p><img class="size-full wp-image-536 aligncenter"
-                                src="../../storage.googleapis.com/stateless-www-seo-for-dummies/2018/12/6cd0d762-topcommenters-settings.png"
-                                alt="" width="520" height="350" /></p>
-                        <hr />
-                        <h2>8. Letzte Kommentare anzeigen</h2>
-                        <p>Wenn Benutzer Ihre Startseite betrachten, wissen sie nicht, was im Kommentarbereich Ihres
-                            Blogs passiert. Zum Beispiel kann es zu einer hitzigen Debatte über einen neuen Beitrag
-                            kommen, in den ein Benutzer eingreifen möchte.</p>
-                        <p>Sie können Benutzern die Möglichkeit bieten, aktuelle Diskussionen anzuzeigen, indem Sie die
-                            neuesten Kommentare zu Ihrer Site anzeigen.</p>
-                        <p>Gehen Sie einfach zu <strong>Darstellung »Widgets</strong> und fügen Sie das Widget Letzte
-                            Kommentare zu einer Seitenleiste hinzu.</p>
-                        <p><img class="size-full wp-image-537 aligncenter"
-                                src="../../storage.googleapis.com/stateless-www-seo-for-dummies/2018/12/e9fbeb5b-recentcomments.jpg"
-                                alt="" width="520" height="294" /></p>
-                        <hr />
-                        <h2>9. Benutzern erlauben, Kommentare zu bewerten und zu teilen</h2>
-                        <p>Das Standard-Kommentarsystem von WordPress ist einfach und unkompliziert. Es verfügt nicht
-                            über die sozialen, Gamification- und Benutzerbindungselemente einer modernen
-                            Diskussionsplattform. Sie können diese Funktionen jedoch mithilfe von Plugins problemlos in
-                            WordPress einfügen.</p>
-                        <p>Einfach das <a href="https://decomments.com/?ref=49">De: comments Plugin</a> installieren und
-                            aktivieren . Nach der Aktivierung verwandelt das Plugin Ihr WordPress-Kommentarsystem auf
-                            wunderbare Weise in eine spannende Diskussionsplattform.</p>
-                        <p><img class="alignnone size-full wp-image-538"
-                                src="../../storage.googleapis.com/stateless-www-seo-for-dummies/2018/12/cee7c1be-decomments.jpg"
-                                alt="" width="520" height="258" /></p>
-                        <p>De: comments beinhaltet Funktionen wie Voting, User Awards, Social Sharing,
-                            Kommentarabonnement und vieles mehr. Dadurch werden Sie keine weiteren Plugins installieren
-                            müssen, die in diesem Artikel erwähnt werden.</p>
-                        <hr />
-                        <h2>10. Benachrichtigen Sie die Benutzer, wenn ihr Kommentar genehmigt wurde</h2>
-                        <p>Wenn ein Benutzer einen Kommentar in Ihrem Blog übergibt, zeigt WordPress ihm, dass sein
-                            Kommentar auf die Moderation wartet. Die Benutzer haben keine Ahnung, wann Sie ihren
-                            Kommentar genehmigen oder löschen.</p>
-                        <p>Sie erhöhen sowohl Ihre Seitenzugriffe als auch die Benutzerinteraktion, indem Sie die
-                            Benutzer einfach darüber informieren, wenn ihr Kommentar genehmigt wurde.</p>
-                        <p>Installieren und aktivieren Sie einfach das Plugin &#8220; <a title="Kommentar genehmigt"
-                                href="https://wordpress.org/plugins/comment-approved/">Comment
-                                Approved&#8220;</a> . Gehen Sie nach der Aktivierung zur Seite <strong>Einstellungen
-                                »Kommentar genehmigt</strong> , um das Plugin zu konfigurieren.</p>
-                        <p><img class="alignnone size-full wp-image-539"
-                                src="../../storage.googleapis.com/stateless-www-seo-for-dummies/2018/12/e8b27f05-comment-approved-settings.jpg"
-                                alt="" width="520" height="339" /></p>
-                        <hr />
-                        <h2>11. Antworten Sie auf Benutzerkommentare</h2>
-                        <p>Dies sollte selbstverständlich sein, aber wenn Sie weitere Kommentare wünschen, müssen Sie
-                            aktiv an den Diskussionen auf Ihrer Website teilnehmen.</p>
-                        <p>Dies bedeutet, dass Sie auf so viele Benutzerkommentare wie möglich antworten müssen.</p>
-                        <p>Sie können Kommentare, auf die Sie antworten können, ganz einfach durchsehen und aktivieren,
-                            indem Sie das <a title="DX unbeantwortete Kommentare"
-                                href="https://wordpress.org/plugins/dx-unanswered-comments/" target="_blank"
-                                rel="nofollow noopener noreferrer">DX-Plug-In für unbeantwortete
-                                Kommentare</a> installieren und aktivieren . Sie können dann filtern und Kommentare
-                            anzeigen, die nicht beantwortet wurden.</p>
-                        <p>Wenn Ihnen dieser Artikel gefallen hat, teilen Sie unseren Beitrag mit anderen oder besuchen
-                            Sie uns bei <a
-                                href="https://www.facebook.com/Seo-for-dummiesde-1048023098713360/">Facebook</a>. Sie
-                            wollen noch mehr über das Thema Marketing, SEO und WordPress erfahren? Komme Sie in unsere
-                            interaktive Facebook Lerngruppe: <a
-                                href="https://www.facebook.com/groups/2386982427984064/">SEO for Dummies – Seo einfach
-                                erklärt</a> </p>
+                        <!-- hier beginnt unser Text der Seite pages.php -->
+                        <?php if (have_posts()) : while(have_posts()) : the_post();?>
+                        <?php the_content();?>
+                        <?php endwhile; endif;?>
+                        <!-- hier endet der Text unserer Seite pages.php -->
                     </div>
                     <div class="navigation-top">
                         <div class="d-sm-flex justify-content-between text-center">
                             <ul class="social-icons">
                                 <p>Teile den Beitrag:</p>
-                                <li><a
-                                        href="https://www.facebook.com/sharer/sharer.php?u=https://www.codingtutorial.de/11-moeglichkeiten-mehr-kommentare-zu-ihren-wordpress-blogbeitraegen-zu-erhalten/"><i
-                                            style="font-size: 48px; color: Dodgerblue;"
-                                            class="fab fa-facebook-square"></i></a></li>
+                                <li>
+                                    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink();?>">
+                                    <i class="fab fa-facebook"></i>
+                                </a>
+                                </li>
                                 <li><a href="whatsapp://send?text=Schau dir diesen Artikel an"
                                         data-action="share/whatsapp/share"><i style="font-size: 48px; color: #25d366"
                                             class="fab fa-whatsapp"></i></a></li>
